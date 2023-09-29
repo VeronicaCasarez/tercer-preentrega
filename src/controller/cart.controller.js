1cdcc0e7b24d4a790af222ad1141f341129cd3ce
Veronica Casarez <casaresveronica54@gmail.com>
2023-09-29 20:05:51 -0300

    tercer-preentrega

--- src/controller/cart.controller.js
+++ src/controller/cart.controller.js
@@ -1,5 +1,7 @@
 import { CARTDAO } from "../dao/index.js";
 import { TICKETDAO } from "../dao/index.js";
+import { PRODUCTDAO } from "../dao/index.js";
+
 
 async function saveCart(req,res){
     const cart= req.body;
@@ -29,21 +31,68 @@ async function updateCart(req,res){
    
 }
 
-async function generatedTicket(req,res){
-    const user=req.user;
-    const cid=req.params.cid;
-    const cartId = await CARTDAO.getCartId(cid);
-    const randomCode = getRandomInt(1000, 9999); 
-    
-    const newTicket = {
-        code:randomCode,
+async function generatedTicket(req, res) {
+    try {
+      const user = req.user;
+      const cid = req.params.cid;
+      const cart = await CARTDAO.getCartId(cid);
+      const randomCode = getRandomInt(1000, 9999);
+  
+      // Recorre los productos en el carrito
+      for (const item of cart.products) {
+        const productId = item.product;
+        const quantity = item.quantity;
+  
+        // Verifica si hay suficiente disponibilidad
+        const product = await PRODUCTDAO.getById(productId);
+  
+        if (!product) {
+          return res.status(404).json({ message: "Producto no encontrado" });
+        }
+  
+        if (product.availability < quantity) {
+          return res.status(400).json({ message: "No hay suficiente disponibilidad para algunos productos en el carrito" });
+        }
+  
+        // Descuenta la cantidad del producto
+        product.availability -= quantity;
+  
+        // Guarda la actualización en la base de datos
+        await product.save();
+      }
+  
+      // Crea el nuevo ticket después de descontar la disponibilidad
+      const newTicket = {
+        code: randomCode,
         purchase_datetime: new Date(),
-        amount:cartId.total,
+        amount: cart.total,
         purchaser: user.user.user.email
+      };
+  
+      const ticket = await TICKETDAO.newTicket(newTicket);
+  
+      res.json({ message: "Ticket generado exitosamente", ticket });
+    } catch (error) {
+      console.error("Error al generar el ticket:", error);
+      res.status(500).json({ message: "Error interno del servidor" });
     }
-    const ticket = TICKETDAO.newTicket(newTicket)
-    res.send( ticket)
-}
+  }
+  
+// async function generatedTicket(req,res){
+//     const user=req.user;
+//     const cid=req.params.cid;
+//     const cart = await CARTDAO.getCartId(cid);
+//     const randomCode = getRandomInt(1000, 9999); 
+    
+//     const newTicket = {
+//         code:randomCode,
+//         purchase_datetime: new Date(),
+//         amount:cart.total,
+//         purchaser: user.user.user.email
+//     }
+//     const ticket = TICKETDAO.newTicket(newTicket)
+//     res.send( ticket)
+// }
 
 
 function getRandomInt(min, max) {
